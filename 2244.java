b02c1a9e5bfc8be1795e84e51aa077c23820185c
areum Han <39939533+reumii@users.noreply.github.com>
2023-01-04 14:34:24 +0900

    Update 2244.java

--- 2244.java
+++ 2244.java
@@ -1,5 +1,6 @@
 // 2244. Minimum Rounds to Complete All Tasks
 class Solution {
+	// 1번째 풀이
     public int minimumRounds(int[] tasks) {
 		HashMap<Integer,Integer> map = new HashMap();
 		for(int i=0; i<tasks.length;i++) {
@@ -29,4 +30,22 @@ class Solution {
 
 		return -1;
 	}
+	
+	// 2번째 풀이
+	public int minimumRounds2(int[] tasks) {
+		HashMap<Integer,Integer> map = new HashMap();
+		for(int t : tasks) map.put(t, map.getOrDefault(t, 0) + 1);
+		
+		int roundCnt = 0;
+		for(int cnt : map.values()) {
+			if(cnt<=1) return -1;
+			roundCnt += cnt/3;
+			if(cnt%3!=0) roundCnt++;
+			// 3으로 나눴을 때 
+			// 나머지가 1 -> 3tasks하는 라운드를 하나 줄이고 2tasks하는 라운드가 두번 추가됨 => 1번 추가
+			// 나머지가 2 -> 2tasks하는 라운드가 하나 추가됨 => 1번 추가
+		}
+		
+        return roundCnt;
+    }
 }
