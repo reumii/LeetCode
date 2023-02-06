da80ba3e83ce3b3988c2bc19393c8b591147c527
areum Han <39939533+reumii@users.noreply.github.com>
2023-02-06 11:06:35 +0900

    1470. Shuffle the Array

---
+++ 1470.java
@@ -0,0 +1,13 @@
+// 1470. Shuffle the Array
+class Solution {
+    public int[] shuffle(int[] nums, int n) {
+        int[] answer = new int[nums.length];
+		
+		for(int i=0; i<n; i++) {
+			answer[i*2] = nums[i];
+			answer[i*2+1] = nums[n+i];
+		}
+		
+        return answer;
+    }
+}
